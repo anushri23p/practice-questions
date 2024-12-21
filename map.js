b2a3231cf5094bed2786b4e130f09702511cebb4
anushri23p <162800663+anushri23p@users.noreply.github.com>
2024-12-21 17:43:53 +0530

    length of strings completed

--- map.js
+++ map.js
@@ -5,10 +5,16 @@ const squaresOf = function (numbers) {
   });
 };
 
-console.log(squaresOf([1, 2, 3, 4]));
+// console.log(squaresOf([1, 2, 3, 4]));
 
 // lengths of ["apple", "banana", "kiwi"] => [5, 6, 4]
-const lengthsOf = function (strings) { };
+const lengthsOf = function (strings) {
+  return strings.map(function (word) {
+    return word.length;
+  });
+};
+
+console.log(lengthsOf(["a", "bb", "ccc", "dddd"]));
 
 // uppercase of ["hello", "world"] => ["HELLO", "WORLD"]
 const uppercaseOf = function (strings) { };
