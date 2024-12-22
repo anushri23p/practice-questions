0e366068b68701fbed5dd0cf85964bae6413a1a4
anushri23p <162800663+anushri23p@users.noreply.github.com>
2024-12-22 22:49:13 +0530

    filter long word completed

--- filter.js
+++ filter.js
@@ -8,14 +8,20 @@ const filterEvenNumbers = function (numbers) {
   return numbers.filter(isEven);
 };
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
-
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
 
 //-----------------------------------------------------------------------------
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] 
 // => ["banana"]
-const filterLongWords = function (words) { };
+const filterLongWords = function (words) {
+  return words.filter(function (word) {
+    return word.length > 5;
+  });
+};
+
+console.log(filterLongWords(["apple", "banana", "kiwi", "grape", "aaaaaaaaaa"]));
+
 
 //-----------------------------------------------------------------------------
 
